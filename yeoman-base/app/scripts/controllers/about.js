c08fe5efaa74f344b4393822797ec12616cb4270
Sunny Shah <shahsunny712@gmail.com>
2017-05-18 01:16:43 -0500

    Addedc new folder to test with yeoman generated app

---
+++ yeoman-base/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanBaseApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yeomanBaseApp
+ */
+angular.module('yeomanBaseApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ yeoman-base/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanBaseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yeomanBaseApp
+ */
+angular.module('yeomanBaseApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
